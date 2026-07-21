da8f88d3967a4924a098e7ca1c18c245b5d1313c
piglet <2792904471@qq.com>
2026-07-21 09:54:46 +0800

    print page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintpte(pagetable_t pagetable, int indent)
+{
+  for(int i=0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 pa = PTE2PA(pte);
+      for(int j=0; j < indent; j++){
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+        // this PTE points to a lower-level page table.
+        vmprintpte((pagetable_t)pa, indent + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintpte(pagetable, 1);
+}
