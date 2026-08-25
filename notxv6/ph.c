dccea1cad58bbb23bbb7ceb41d7ca515cb038213
piglet <2792904471@qq.com>
2026-08-25 18:09:11 +0800

    using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t bucket_lock[NBUCKET];
+
 
 double
 now()
@@ -43,6 +45,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(bucket_lock + i);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -54,7 +57,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(bucket_lock + i);
 }
 
 static struct entry*
@@ -116,6 +119,10 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for(int i=0; i < NBUCKET; i++) {
+    pthread_mutex_init(bucket_lock + i, NULL);
   }
 
   //
